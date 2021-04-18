d0c05258e7d94a5008adcf7de5cd30c4b17f6a98
Rafal Przetakowski <rafal.p@beeflow.co.uk>
2021-04-18 10:29:15 +0100

    unittests

--- tests/test.sql
+++ tests/test.sql
@@ -2,23 +2,50 @@
 BEGIN;
 
 -- Plan how many tests you want to run.
-SELECT tap.plan(1);
+SELECT tap.plan(90);
 
 -- Run the tests.
 SELECT tap.pass('My test passed, w00t!');
-
--- Finish the tests and clean up.
-CALL tap.finish();
-ROLLBACK;
-
-BEGIN;
-SELECT tap.plan(5);
 
 SELECT tap.has_table(DATABASE(), 'book', 'Table book exists!');
 SELECT tap.has_table(DATABASE(), 'author', 'Table author exists!');
 SELECT tap.has_table(DATABASE(), 'user', 'Table user exists!');
 SELECT tap.has_table(DATABASE(), 'book_author', 'Table book_author exists!');
 SELECT tap.has_table(DATABASE(), 'book_status', 'Table book_status exists!');
+
+insert into `user` (first_name_id, last_name_id, email)
+values (f_add_first_name('Heniek'), f_add_last_name('Olbromski'), 'heniek.o@beeflow.co.uk');
+
+INSERT INTO book(book_title, book_isbn)
+VALUES ('Przygody Heńka O', '1-1153111');
+insert into book_copy (book_id)
+select book_id
+from book
+where book_title = 'Przygody Heńka O';
+
+select id
+from book_status
+where name = 'Dostępna';
+
+select tap.ok((select id from book_status where name = 'Dostepna'), 'Istnieje status dostępna');
+
+select tap.eq(
+               (select status_id from book_copy where book_id = (select book_id from book where book_title = 'Przygody Heńka O')),
+               (select id from book_status where lower(name) = 'dostepna'),
+               'Domyślnie kopia książki jest dostępna'
+           );
+
+insert into user_book_rent (bc_id, user_id)
+values (
+        (select book_id from book where book_title = 'Przygody Heńka O'),
+        (select id from `user` where email = 'heniek.o@beeflow.co.uk')
+);
+
+select tap.eq(
+               (select status_id from book_copy where book_id = (select book_id from book where book_title = 'Przygody Heńka O')),
+               (select id from book_status where lower(name) = 'wypozyczona'),
+               'Wypożyczenie zmienia status książki na wypożyczona'
+           );
 
 CALL tap.finish();
 ROLLBACK;
